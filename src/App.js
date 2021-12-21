50b12ac04343abf5c692bf1db5510144aedd2e9e
settc <tewhittle@gmail.com>
2021-12-21 13:22:18 -0500

    Make json request and render response to page through lifecycle method

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import { Component } from "react"
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "1"
-        },
-        {
-          name: "Dracula",
-          id: "2"
-        },
-        {
-          name: "Wolf Man",
-          id: "3"
-        }
-      ]
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState( { monsters: users } ))
   }
   
   render() {
